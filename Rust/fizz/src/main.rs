d0ccf8a582a0826edd51f9f079ea092c5916409f
Marius Ciubotariu <marius.ciubotariu@gmail.com>
2019-10-06 09:08:00 +0100

    rust reference implementation but slower

--- Rust/fizz/src/main.rs
+++ Rust/fizz/src/main.rs
@@ -1,23 +1,19 @@
+// Allows us to avoid unnecessary allocations
+use std::borrow::Cow;
+
 fn main() {
     let num =
         match std::env::args().skip(1).next() {
             None => 5,
             Some (x) => x.parse::<u32>().unwrap()
         };
+
     println!("Number {}", num);
-    fizz(num);
-}
 
-fn fizz(n: u32){
-    for x in 1..(n+1) {
-        let output =
-            match (x % 3 == 0, x % 5 == 0) {
-                (true, true) =>  "FizzBuzz".to_string(),
-                (true, false) => "Fizz".to_string(),
-                (false, true) => "Buzz".to_string(),
-                (false, false) => format!("{}", x)
-            };
-
-        println!("{}", output)
-    }
+    (1..(num+1)).map(|n| match (n%3, n%5) {
+        (0, 0) => "FizzBuzz".into(),
+        (0, _) => "Fizz".into(),
+        (_, 0) => "Buzz".into(),
+        _ => Cow::from(n.to_string())
+    }).for_each(|n| println!("{}", n));
 }
